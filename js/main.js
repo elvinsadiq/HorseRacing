c547f92c3fcf13edcd60ec02eb3a1b7b79a4fd47
elvinsadiq <74909791+elvinsadiq@users.noreply.github.com>
2023-08-18 01:07:39 +0400

    Added javascript

--- js/main.js
+++ js/main.js
@@ -1,9 +1,13 @@
 $(document).ready(function(){
+
     const names = ["Blazer", "Franklin", "Blacksmith", "Tennesee", "Buckeye", "Bronco"]
 
-    $("#btncount").click(function() {
+    $("#btnCount").click(function() {
         const count = $("#count").val();
 
+        $(".h4").removeClass("d-none");
+        $(".h4").text("Race Will Begin Shortly");
+        
         if(isNaN(count)){
             alert("Please only enter numerical values");
             $("#count").val("");
@@ -17,11 +21,56 @@ $(document).ready(function(){
 
         let templates = []
         for(let i = 1; i <= count; i++){
-            templates.push(`<div class="row"> <div class="col"><img src="./img/h${i}" alt=""> </div></div>`)
+            templates.push(`<div class="row"> <div class="col"><img src="./img/h${i}.gif" alt=""> </div></div>`)
         }
+
+        document.getElementById("container").innerHTML = templates.join("<br>");
+        $("#count").val("");
 
      });
 
-    $("#btnStart").click(function() { })
+     function random(min, max) {
+        return Math.floor(Math.random() * (max - min) + min);
+    }
 
+    $("#btnStart").click(function () {
+        var horses = document.getElementsByTagName("img");
+        if (horses.length == 0) {
+            alert("Please Enter Value");
+            return;
+        }
+        var lblFinish = $(".finish-label").position().left;
+
+        var timer = setInterval(() => {
+            $.each(horses, (key, value) => {
+
+                const itemWidht = $(value).position().left + $(value).width();
+
+
+                let _counter = 0;
+                let _winner = -1;
+                for (let i = 0; i < horses.length; i++) {
+                    if ($(horses[i]).position().left > _counter) {
+                        _counter = $(horses[i]).position().left;
+                        _winner = i;
+                    }
+                } 
+                
+                $(".h4").text(`Horse on lane ${_winner + 1} leads the race: ${names[_winner]}`);
+
+                if (itemWidht > lblFinish) {
+                    clearInterval(timer); 
+                    alert(`Horse on lane ${_winner + 1} won the race: ${names[_winner]}`);
+                    $("img").addClass("d-none");
+                    $(".h4").addClass("d-none");
+                    return;
+                }
+
+                let position = $(value).position().left;
+                $(value).css("left", (position + random(5, 30)));
+
+            });
+        }, (70));
+
+    });
 })
